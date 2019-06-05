7faeeda98ae9045fae3febe070011c26b944da32
592306832@qq.com <592306832@qq.com>
2019-06-05 23:55:32 +0800

    redisstart

---
+++ redisoper-springboot-start/src/main/java/com/redisoper/RedisoperSpringbootStartApplication.java
@@ -0,0 +1,13 @@
+package com.redisoper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisoperSpringbootStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisoperSpringbootStartApplication.class, args);
+    }
+
+}
