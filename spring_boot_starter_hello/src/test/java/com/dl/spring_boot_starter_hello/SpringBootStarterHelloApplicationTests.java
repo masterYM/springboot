a15347edf6249a957863f86a6d09621bcbc34653
592306832@qq.com <592306832@qq.com>
2019-06-06 16:16:12 +0800

    autoconfig

--- spring_boot_starter_hello/src/test/java/com/dl/spring_boot_starter_hello/SpringBootStarterHelloApplicationTests.java
+++ spring_boot_starter_hello/src/test/java/com/dl/spring_boot_starter_hello/SpringBootStarterHelloApplicationTests.java
@@ -1,4 +1,4 @@
-package com.dl;
+package com.dl.spring_boot_starter_hello;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class RedisoperSpringBootAutoconfigurationApplicationTests {
+public class SpringBootStarterHelloApplicationTests {
 
     @Test
     public void contextLoads() {
